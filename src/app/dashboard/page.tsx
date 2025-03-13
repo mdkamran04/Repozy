d2cc5940ee78bc8293adddd4104d2d97296d8086
Mohamed Kamran <mohamedkamran04@gmail.com>
2025-03-13 13:14:02 +0530

    setting up dashboard and routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import React from 'react';
+import { useUser } from '@clerk/nextjs';
+
+const DashboardPage = () => {
+    const {user}=  useUser();
+
+  return (
+    <div>
+       {user?.firstName} 
+      
+    </div>
+  )
+}
+
+export default DashboardPage;
