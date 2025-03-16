a3531de66f184ef716499f455efe8ab624280d2e
Mohamed Kamran <mohamedkamran04@gmail.com>
2025-03-16 11:53:36 +0530

    built the dashboard structure

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,18 +1,50 @@
-"use client"
+"use client";
 
-import React from 'react';
-import { useUser } from '@clerk/nextjs';
+import React from "react";
+
+import useProject from "@/hooks/use-project";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 
 const DashboardPage = () => {
-    const {user}=  useUser();
+  const { project } = useProject();
 
   return (
-    <div>Welcome back , 
-       {user?.firstName}
-       {user?.lastName} 
-      
+    <div>
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        {/* GitHub Link here */}
+        <div className="w-fit items-center rounded-md bg-primary px-2 py-3">
+          <div className="flex items-center">
+            <Github className="size-5 text-white" />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+                This project is linked to{" "}
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  target="_blank"
+                  className="iten-center inline-flex text-white/80 underline"
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className="ml-1 size-4" />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="h-4"></div>
+        <div className="flex items-center gap-4">
+          Team Members Invite brn archive btn
+        </div>
+      </div>
+      <div className="mt-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          Ask question Card Meeting Card
+        </div>
+      </div>
+      <div className="mt-8"></div>
+      Commits logs here
     </div>
-  )
-}
+  );
+};
 
 export default DashboardPage;
