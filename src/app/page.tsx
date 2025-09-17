a6cdad1cc73f074ad37072898668214fd87c89d5
mdkamran04 <mohamedkamran04@gmail.com>
2025-09-17 15:53:56 +0530

    redirect url directly to /dashboard instead of home , sign-out feature to be done later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
 
-export default async function Home() {
-return <Button>Click me</Button>
+// export default async function Home() {
+// return <Button>Click me</Button>
+// }
+
+
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/dashboard");
 }
