433b2a954d1d23280ad734f0fab8ed2b4981ae2e
mdkamran04 <mohamedkamran04@gmail.com>
2025-09-19 22:29:23 +0530

    meeting card created and input taken

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,25 +1,63 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getStorage} from "firebase/storage";
+import { getStorage } from "firebase/storage";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { ca } from "zod/v4/locales";
+import { error } from "console";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBs_McCBwXuTABfVHeblu5tE6tnSagNjDg",
-  authDomain: "gitops-ai-original.firebaseapp.com",
-  projectId: "gitops-ai-original",
-  storageBucket: "gitops-ai-original.firebasestorage.app",
-  messagingSenderId: "181199764094",
-  appId: "1:181199764094:web:e646cf0611fc602ab5f62f",
-  measurementId: "G-FM21X23FLL"
+    apiKey: "AIzaSyBs_McCBwXuTABfVHeblu5tE6tnSagNjDg",
+    authDomain: "gitops-ai-original.firebaseapp.com",
+    projectId: "gitops-ai-original",
+    storageBucket: "gitops-ai-original.firebasestorage.app",
+    messagingSenderId: "181199764094",
+    appId: "1:181199764094:web:e646cf0611fc602ab5f62f",
+    measurementId: "G-FM21X23FLL"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-
 export const storage = getStorage(app);
+
+
+export async function uploadFile(file: File, setProgress?: (progress: number) => void) {
+    return new Promise((resolve, reject) => {
+        try {
+            const storageRef = ref(storage, file.name);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on('state_changed',
+                (snapshot) => {
+                    const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                    if (setProgress) setProgress(progress);
+                    switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                    }
+                }, error => {
+                    reject(error);
+                }, () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+                        resolve(downloadUrl);
+                    });
+                })
+
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+
+
