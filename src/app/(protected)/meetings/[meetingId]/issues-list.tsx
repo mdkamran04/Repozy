8c470b0a79330e082851f3d9660b03f0d0afcd3f
mdkamran04 <mohamedkamran04@gmail.com>
2025-09-30 23:15:02 +0530

    issues card created and designed

--- src/app/(protected)/meetings/[meetingId]/issues-list.tsx
+++ src/app/(protected)/meetings/[meetingId]/issues-list.tsx
@@ -1,48 +1,105 @@
-'use client'
+"use client";
 
-import { ref } from 'firebase/storage'
-import React from 'react'
-import { api, RouterOutputs } from '@/trpc/react'
-import { VideoIcon } from 'lucide-react'
+import { ref } from "firebase/storage";
+import React, { use } from "react";
+import { api, RouterOutputs } from "@/trpc/react";
+import { VideoIcon } from "lucide-react";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardContent,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 type Props = {
-    meetingId: string
-}
-const IssuesList = ({meetingId}:Props) => {
-    const { data: meeting, isLoading } = api.project.getMeetingById.useQuery({ meetingId },{
-        refetchInterval: 5000,
-    });
-    if (isLoading || !meeting) return <div>Loading...</div>
+  meetingId: string;
+};
+const IssuesList = ({ meetingId }: Props) => {
+  const { data: meeting, isLoading } = api.project.getMeetingById.useQuery(
+    { meetingId },
+    {
+      refetchInterval: 5000,
+    },
+  );
+  if (isLoading || !meeting) return <div>Loading...</div>;
   return (
     <>
-    <div className="p-8 ">
-        <div className="mx-auto flex max-w-2xl items-center justify-between gap-x-8 border-b pb-6 lg:mx-0 lg:max-w-none ">
-
-            <div className="flex items-center gap-x-6 ">
-                <div className="rounded-full border bg-white p-3">
-                    <VideoIcon className="h-6 w-6"/>
-                </div>
-                <h1>
-                    <div className="text-sm  leading-6 text-gray-600">Meeting on {""}{meeting.createdAt.toLocaleDateString()}</div>
-                    <div className="mt-1 text-base font-semibold leading-6 text-gray-900">{meeting.name}</div>
-                </h1>
+      <div className="p-8">
+        <div className="mx-auto flex max-w-2xl items-center justify-between gap-x-8 border-b pb-6 lg:mx-0 lg:max-w-none">
+          <div className="flex items-center gap-x-6">
+            <div className="rounded-full border bg-white p-3">
+              <VideoIcon className="h-6 w-6" />
             </div>
+            <h1>
+              <div className="text-sm leading-6 text-gray-600">
+                Meeting on {""}
+                {meeting.createdAt.toLocaleDateString()}
+              </div>
+              <div className="mt-1 text-base font-semibold leading-6 text-gray-900">
+                {meeting.name}
+              </div>
+            </h1>
+          </div>
         </div>
         <div className="h-4">
-            <div className="grid grid-cols-1 gap-2 sm:grid-cols-3">
-                {meeting.issues.map((issue) => (
-                    <IssueCard key={issue.id} issue={issue}/>
-                ))}
-            </div>
+          <div className="grid grid-cols-1 gap-2 sm:grid-cols-3">
+            {meeting.issues.map((issue) => (
+              <IssueCard key={issue.id} issue={issue} />
+            ))}
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-function IssueCard({issue}:{issue :NonNullable<RouterOutputs['project']['getMeetingById']>['issues'][number]}) {
-    return (
-        <div className="rounded-lg border p-4 shadow-sm hover:shadow-md"> Issue Card</div>
-    )
+function IssueCard({
+  issue,
+}: {
+  issue: NonNullable<
+    RouterOutputs["project"]["getMeetingById"]
+  >["issues"][number];
+}) {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{issue.gist}</DialogTitle>
+            <DialogDescription>
+              {issue.createdAt.toLocaleDateString()}
+            </DialogDescription>
+            <p className="text-gray-600">{issue.headline}</p>
+            <blockquote className="mt-2 border-l-4 border-gray-300 bg-gray-50 p-4">
+              <span className="text-sm text-gray-600">
+                {issue.start}-{issue.end}
+              </span>
+              <p className="font-medium italic leading-relaxed text-gray-900">{issue.summary}</p>
+            </blockquote>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Card className="relative">
+        <CardHeader>
+          <CardTitle className="text-xl">{issue.gist}</CardTitle>
+          <div className="border-b"></div>
+          <CardDescription>{issue.headline}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button onClick={() => setOpen(true)}>View Details</Button>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
-export default IssuesList
+export default IssuesList;
